0c61ef679e46fe1b8ab3df12a5d7e5f7b7dcb23e
yuanda@laptop <34027873+yuandahanif@users.noreply.github.com>
2023-03-08 20:44:43 +0700

    feat: :sparkles: add post trpc router

---
+++ src/server/api/routers/post.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  publicProcedure,
+  protectedProcedure,
+} from "~/server/api/trpc";
+
+export const postRouter = createTRPCRouter({
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.post.findMany();
+  }),
+
+  getbyId: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.prisma.post.findFirstOrThrow({ where: { id: input.id } });
+    }),
+});
